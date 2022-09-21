a4ab7f492700176b5d5479bc0d7f187737cf69a9
HP <HP@192.168.1.22>
2022-09-20 20:52:24 -0500

    Primer commit

---
+++ src/main/java/kenyi/ejercicios/E2.java
@@ -0,0 +1,18 @@
+package kenyi.ejercicios;
+import java.util.Scanner;
+
+public class E2 {
+
+    public static void main(String[] args) {
+       //KENYI CONDOR TRUCIOS//
+       //Realizar un programa que permita mostrar en pantalla tu nombre y apellido//
+       String nomb, apell;
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Ingrese su nombre: ");
+       nomb = sc.nextLine();
+       System.out.print("Ingrese sus apellidos: ");
+       apell = sc.nextLine();
+       
+       System.out.println("Bienvenido "+nomb+" "+apell);
+    }  
+}
---
+++ src/main/java/kenyi/ejercicios/E3.java
@@ -0,0 +1,18 @@
+package kenyi.ejercicios;
+import java.util.Scanner;
+
+public class E3 {
+    
+    public static void main(String[] args) {
+        //KENYI CONDOR TRUCIOS//
+        //Realizar un programa que permita calcular la suma de 5 números//
+        float num, suma=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese los numeros: ");
+        for (int i = 0; i < 5; i++) {
+            num = sc.nextFloat();
+            suma = num+suma;
+        }
+        System.out.println("Resultado de la suma es: "+suma);
+    }
+}
